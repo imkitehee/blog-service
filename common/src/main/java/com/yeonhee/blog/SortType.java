c43492b8b7d7b4f82cbe68b86e93f0f8967b9bb5
정연희 <dusgmlggg@naver.com>
2023-03-19 00:13:04 +0900

    common 모듈에 sortType enum을 추가하고, 필요한 디펜던시를 추가하라

---
+++ common/src/main/java/com/yeonhee/blog/SortType.java
@@ -0,0 +1,16 @@
+package com.yeonhee.blog;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum SortType {
+
+    ACCURACY("정확도순", "accuracy", "sim"),
+    RECENCY("최신순", "recency", "date");
+
+    private final String description;
+    private final String inKakao;
+    private final String inNaver;
+}
